9dcb42ce9931c8737fc39831d5514d7f44c41302
Nico Stuurman <nico.stuurman@ucsf.edu>
2020-02-17 15:46:00 -0800

    Almost together

---
+++ js/publications.js
@@ -0,0 +1,40 @@
+var data = [
+  {
+  x: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019],
+  y: [32, 65, 119, 182, 291, 473, 725, 1100, 1570, 2080, 2670, 3520, 4360],
+  name: 'Publications mentioning &mu;Manager',
+  type: 'scatter'
+  }
+];
+var std_font = {
+        size: '32',                                 
+        color: 'white'
+};
+var layout = {
+  paper_bgcolor: 'rgba(255,255,255,0)',
+  plot_bgcolor: 'rgba(255,255,255,0)',
+               font: std_font,
+  xaxis: {
+     title: 'Year',
+     titlefont: std_font,
+     showgrid: false,
+     dtick: 2,
+     tickfont: {
+        size: '20',
+        color: 'white'
+     }
+  },
+  yaxis: {
+     title: '# of publications',
+     titlefont: std_font,
+     color: 'white',
+     range: [0, 5000],
+     tickangle: -90,
+     tickfont: {
+        size: '20',
+        color: 'white'
+     }
+  }
+};
+
+Plotly.newPlot('publications', data, layout);
---
+++ js/registrations.js
@@ -0,0 +1,41 @@
+var data = [
+  {
+  x: [2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
+  y: [920, 2131, 3895, 6087, 8953, 12531, 16833, 21607, 26507, 30997, 35974, 40482, 45172],
+  name: 'Cumulative Registrations',
+  type: 'scatter'
+  }
+];
+var std_font = {
+        size: '32',                                 
+        color: 'white'
+};
+var layout = {
+  paper_bgcolor: 'rgba(255,255,255,0)',
+  plot_bgcolor: 'rgba(255,255,255,0)',
+               font: std_font,
+  xaxis: {
+     title: 'Year',
+     titlefont: std_font,
+     showgrid: false,
+     dtick: 2,
+     tickfont: {
+        size: '20',
+        color: 'white'
+     },
+     title_standoff: 200
+  },
+  yaxis: {
+     title: '# of Registrations',
+     titlefont: std_font,
+     color: 'white',
+     range: [0, 50000],
+     tickfont: {
+        size: '20',
+        color: 'white'
+     },
+     title_standoff: 100
+  }
+};
+
+Plotly.newPlot('registrations', data, layout);
